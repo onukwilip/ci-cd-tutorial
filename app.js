664840921139d9ff9ee4981040d3b124d5af40c8
onukwilip <onukwilip@gmail.com>
2024-12-14 18:30:48 +0100

    Committing changes

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.get("/", (req, res, next) => {
   res.send(`
     <html>
       <head>
-        <title>Node Js Web Server</title>
+        <title>Node Js Web Server V2</title>
       </head>
     </html>
     <body>
-      <h1>Hello world! I'm a Node/Express Js web server...</h1>
+      <h1>Hello world! I'm a Node/Express Js web server version 2...</h1>
     </body>
     `);
   next();
